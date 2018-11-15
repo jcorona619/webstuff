45e2b91821dd1aba66b0f0ba7e0629bea9e50f66
jcorona619 <josh.corona@outlook.com>
2018-11-15 15:39:56 -0800

    fully connected to html page, php, and ,sql. Register user

--- register_user.php
+++ register_user.php
@@ -1,33 +1,37 @@
 <?php
 
+//include('register.html');
+
 echo "Register Page";
 
 echo "<br>";
 echo "<br>";
 
-//$servername = "localhost:3306";
-//$username = "root";
-//$password = "Qwzztop1!";
-//$dbname = "socialmediasite";
 define("DB_HOST", "localhost:3306");
 define("DB_USR", "root");
 define("DB_PSWD", "Qwzztop1!");
 define("DB_NAME", "socialmediasite");
 
-//$conn = new mysqli($servername, $username, $password,$dbname);
 $conn = new mysqli(DB_HOST,DB_USR,DB_PSWD,DB_NAME);
+
+/*
+echo $_POST["user_id"];
+echo $_POST["fname"];
+echo $_POST["lname"];
+echo $_POST["email"];
+echo $_POST["pswrd"];
+*/
 
 if($conn->connect_error){
 	die("connection failed:" .$conn->connect_error);
 }
 
-//mysql_select_db("socialmediasite");
-
-$sql = "INSERT INTO users(user_id,email,password,sex,age)
-			VALUES('hellod','hellod@site.com','33322','F',21)";
+$sql = "INSERT INTO users(user_id,fname,lname,email,password)
+			VALUES('$_POST[user_id]','$_POST[fname]',
+				   '$_POST[lname]','$_POST[email]','$_POST[pswrd]')";
 
 if($conn->query($sql) == TRUE){
-	echo "New record recorded successfully";
+	echo "New record created successfully";
 	echo "<br>";
 	echo "<br>";
 } else {
