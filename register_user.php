58a5c4d831b42d5c46b7c40b7c568efeda3fea4d
jcorona619 <josh.corona@outlook.com>
2018-11-16 02:52:58 -0800

    not much of nothin

--- register_user.php
+++ register_user.php
@@ -10,25 +10,22 @@ echo "<br>";
 define("DB_HOST", "localhost:3306");
 define("DB_USR", "root");
 define("DB_PSWD", "Qwzztop1!");
-define("DB_NAME", "socialmediasite");
+define("DB_NAME", "social_media_site");
 
 $conn = new mysqli(DB_HOST,DB_USR,DB_PSWD,DB_NAME);
 
-/*
-echo $_POST["user_id"];
-echo $_POST["fname"];
-echo $_POST["lname"];
-echo $_POST["email"];
-echo $_POST["pswrd"];
-*/
+$username = test_input($_POST['username']);
+$fname = test_input($_POST['fname']);
+$lname = test_input($_POST['lname']);
+$email = test_input($_POST['email']);
+$password = md5($_POST['pswrd']);
 
 if($conn->connect_error){
 	die("connection failed:" .$conn->connect_error);
 }
 
-$sql = "INSERT INTO users(user_id,fname,lname,email,password)
-			VALUES('$_POST[user_id]','$_POST[fname]',
-				   '$_POST[lname]','$_POST[email]','$_POST[pswrd]')";
+$sql = "INSERT INTO users(username,fname,lname,email,password)
+			VALUES('$username','$fname','$lname','$email','$password')";
 
 if($conn->query($sql) == TRUE){
 	echo "New record created successfully";
@@ -41,5 +38,14 @@ if($conn->query($sql) == TRUE){
 }
 
 echo "Connection Successful!";
+
+/* Handles unnecessary characters (spaces,tabs,etc), gets rid of backslashes
+     and catches and html characters from executing in the form */
+  function test_input($data){
+  	$data = trim($data);
+  	$data = stripslashes($data);
+  	$data = htmlspecialchars($data);
+  	return $data;
+  }
 
 ?>
